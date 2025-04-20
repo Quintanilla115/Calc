aa89b9e729930bccc1dbe9846028d2e011850c41
Alec Quintanilla <23916616+Quintanilla115@users.noreply.github.com>
2025-04-20 16:07:14 -0500

    nuked JS, kept essentials

--- calc.js
+++ calc.js
@@ -1,49 +1,27 @@
+shouldResetScreen = false;
 
-let CurrentDisplay = ''
-let PreviousDisplay = ''
-let shouldResetScreen = false
-let Opperator = null
 
-const numberButtons = document.querySelectorAll('[data-number]')
 const buttons = document.querySelectorAll("button");
-const CurrentOperatorScreen = document.getElementById('CurrentDisplay')
-const ClearButton = document.getElementById('Clear')
+const CurrentoperatorScreen = document.getElementById('CurrentDisplay');
+const Numbers = document.querySelectorAll(".number");  
 
 
-
-buttons.forEach((button) => {
-  
+Numbers.forEach((button) => {
   button.addEventListener("click", (e) => {
-    appendNumber(e.target.textContent);
+    appendNumber(e.target.textContent); 
   });
 });
 
 function appendNumber(number) {
-  CurrentDisplay += number
-
-  console.log(typeof CurrentDisplay);
-  console.log(CurrentDisplay)
-  CurrentOperatorScreen.textContent +=number
+  if (shouldResetScreen) {
+    CurrentoperatorScreen.textContent = number;
+    shouldResetScreen = false;
+  } else {
+    CurrentoperatorScreen.textContent += number;
+  }
 }
 
-function updateScreen(){}
 
 
-function appendNumber1(number){
-  if (number === 1||2||3||4||5||6||7||8||9||0){
-    appendNumber
-  }
-  if (number === '*' || '/' || '-'|| '+' ){
-    if (number === '*'){
-      Opperator = multiply
-    }
-    if (number === '/'){
-      Opperator = divide
-    }
-    if (number === '-'){
-      Opperator = subtract
-  }
-  if (number === '+'){
-    Opperator = addition
-  }
-}}
+
+
